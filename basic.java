aa0db4a866de03688e7c0b3cabe6fc1e2dbdffd4
Narramla15 <163990663+Narramla15@users.noreply.github.com>
2025-04-10 09:29:42 +0530

    Update basic.java

--- basic.java
+++ basic.java
@@ -2,6 +2,6 @@ public class basic
 {
    public static void main(String args[])
    {
-      System.out.println("Hello world I am vinesh");
+      System.out.println("Hello world I am vinesh from cse deparment Rgukt basar");
    }
 }
